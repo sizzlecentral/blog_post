685579785c6cb60b4d41dd64ec7182f0a0f7ae90
Don Cooper <don_cooper@hotmail.com>
2017-09-05 14:57:19 -0400

    API fetch added to project

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,47 @@
 import React, {Component} from 'react';
 
 class Home extends Component {
-  render () {
-    return (
-      <div>
-        <h1>Home</h1>
-        <p>Thanks for visiting my awesome website! Thank you Geocities for hosting me! YEAHH!!!!</p>
-      </div>
-    )
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      shakeSpeare: ""
+    }
+  }
+
+  componentDidMount() {
+    // save a reference to `this` because the value of `this` will change
+    // inside the different callback functions.
+    var base = this;
+
+    // fetch a poem
+    let poemApi = 'http://ShakeItSpeare.com/api/poem';
+    fetch(poemApi)
+      .then((response) => {
+        return response.json()
+      }).then((json) => {
+          base.setState({ shakeSpeare: json.poem });
+      }).catch((ex) => {
+        console.log('An error occured while parsing!', ex)
+      });
+  }
+
+  render() {
+       let poetry = this.state.shakeSpeare;
+     if (this.state.shakeSpeare){
+       return (
+         <div>
+           <h1>My favorite Shakespeare poem:</h1>
+           {poetry}
+         </div>
+       );
+     }
+     return (
+       <div>
+         <h1>My favorite Shakespeare poem:</h1>
+         Loading...
+       </div>
+     );
   }
 }
 
