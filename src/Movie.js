4747f4bbd5bb4a2bfd22931b9e2b01ab3dd08e82
Don Cooper <don_cooper@hotmail.com>
2017-09-11 14:34:40 -0400

    Revert "Fetch movies and weather added to project"
    This reverts commit db9a61607e32cc4e6beca88a64462b18ecaa85f6.

--- src/Movie.js
+++ src/Movie.js
@@ -1,70 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Movie extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchQuery: '',
-      movieTitle: '',
-      movieDirector: '',
-      movieSummary: '',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({searchQuery: event.target.value});
-    console.log('Your search query is' + this.state.searchQuery);
-  }
-
-  handleSubmit(event) {
-
-    let base = this;
-
-    let withSpaces = this.state.searchQuery.replace(/%20/g, " ");
-
-    this.setState({searchQuery: withSpaces});
-
-    console.log('You are adding the following movie: ' + this.state.searchQuery);
-
-    fetch('http://netflixroulette.net/api/api.php?title=' + this.state.searchQuery)
-      .then(function(response) {
-        console.log(response);
-        return response.json()
-      }).then(function(json) {
-        console.log('Parsed JSON', json)
-        base.setState({
-          movieTitle: json.show_title,
-          movieDirector: json.director,
-          movieSummary: json.summary,
-         })
-      }).catch(function(ex) {
-        console.log('Parsing JSON failed', ex)
-        alert('Error! ' + ex);
-      });
-
-    event.preventDefault();
-  }
-
-  render() {
-
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Get this movie!" />
-
-        <p>{this.state.movieTitle}</p>
-        <p>Directed by {this.state.movieDirector}</p>
-        <p>{this.state.movieSummary}</p>
-      </form>
-    );
-  }
-}
+const Movie = () => (
+  <div>
+    <h2>The BEST movie</h2>
+    <p>The Empire Strikes Back</p>
+  </div>
+)
 
 export default Movie;
